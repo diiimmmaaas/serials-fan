0f16e24ff59547707e5def6e986c06c3a5ad88d7
sokolov_d <sokolov_d@elgrow.ru>
2023-10-16 13:06:48 +0300

    create modal

--- main.js
+++ main.js
@@ -1,25 +1,50 @@
-const carousel = document.querySelector(".serialsFun-carousel");
-const prevButton = document.querySelector(".serialsFun-prev-button");
-const nextButton = document.querySelector(".serialsFun-next-button");
+document.addEventListener("DOMContentLoaded", function() {
+  const carousel = document.querySelector(".serialsFun-carousel");
+  const prevButton = document.querySelector(".serialsFun-prev-button");
+  const nextButton = document.querySelector(".serialsFun-next-button");
 
-let currentIndex = 0;
+  let currentIndex = 0;
+  const itemWidth = carousel.querySelector(".serialsFun-carousel-item").offsetWidth; // Ширина блока + расстояние между блоками
 
-prevButton.addEventListener("click", () => {
-  if (currentIndex > 0) {
-    currentIndex -= 4;
-    moveCarousel();
+  console.log('itemWidth', itemWidth);
+
+  function updateCarousel() {
+    const offset = currentIndex * -itemWidth;
+    carousel.style.transform = `translateX(${offset}px)`;
   }
+
+  function showNext() {
+    currentIndex = Math.min(currentIndex + 4, 11);
+    updateCarousel();
+  }
+
+  function showPrev() {
+    currentIndex = Math.max(currentIndex - 4, 0);
+    updateCarousel();
+  }
+
+  prevButton.addEventListener("click", showPrev);
+  nextButton.addEventListener("click", showNext);
 });
 
-nextButton.addEventListener("click", () => {
-  if (currentIndex + 4 < carousel.children.length) {
-    currentIndex += 4;
-    moveCarousel();
-  }
-});
+var openModalBtnsWithClassName = document.getElementsByClassName("serialsFunOpenModalBtn");
+var closeModalBtn = document.getElementById("serialsFunCloseModalBtn");
+var modal = document.getElementById("serialsFunModal");
 
-function moveCarousel() {
-  carousel.style.transform = translateX(-${currentIndex * 160}px); // 160px = ширина изображения + отступ
+
+for (let i = 0; i < openModalBtnsWithClassName.length; i++) {
+  const openModalBtn = openModalBtnsWithClassName[i]
+  openModalBtn.addEventListener("click", function() {
+    modal.style.display = "block";
+  });
 }
 
-moveCarousel();
+closeModalBtn.addEventListener("click", function() {
+  modal.style.display = "none";
+});
+
+window.addEventListener("click", function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+});
